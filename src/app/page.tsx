795075078be3216a9289149a0b905adb3c99f711
Nitin Singh <vortechcorp@gmail.com>
2025-08-05 22:11:44 +0530

    added inngest for background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
 export default function Home() {
-  return <div>hello</div>;
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: "Test@test.com" })}>
+        Invoke Backgroung job
+      </Button>
+    </div>
+  );
 }
