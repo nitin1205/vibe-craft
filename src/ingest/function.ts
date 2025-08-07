09fad55b54c1b7f8a22d014221a77ff9a9c73adf
Nitin Singh <vortechcorp@gmail.com>
2025-08-07 21:45:36 +0530

    created ai background jobs

--- src/ingest/function.ts
+++ src/ingest/function.ts
@@ -1,10 +1,24 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable, and efficient code. Ypu write simple and clear Next.js and React snippets.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `write the following snippet: ${event.data.value}`
+    );
+
+    console.log("Summarized output:", output);
+
+    return { output };
   }
 );
